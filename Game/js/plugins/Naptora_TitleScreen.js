14a4892b32798b0e368f1732f8b24b453ca00842
Hitorenti <zmilingrs@gmail.com>
2022-11-20 16:42:56 -0600

    Tittle screen plugin added

---
+++ Game/js/plugins/Naptora_TitleScreen.js
@@ -0,0 +1,32 @@
+/*:
+ *
+ * @plugindesc Title screen menu
+ * @author Naptora
+ * 
+ * @param Background
+ * @desc Image in bottom of scene
+ * @default MenuBackground
+ * @require 1
+ * @dir img/menu/
+ * @type file
+ * 
+ * @param Start key
+ * @desc Key for show menu
+ * @default z
+ * 
+ * @param Title Image
+ * @desc Image to use as title
+ * @default logo
+ * @require 1
+ * @dir img/
+ * @type file
+ * 
+ */
+
+var params = PluginManager.parameters("Naptora_TitleScreen");
+
+(function(){
+
+
+    
+})();
