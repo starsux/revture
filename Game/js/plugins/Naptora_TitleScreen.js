82e50e7bdc86db813e99b7490d2dd45f73475ee1
Hitorenti <zmilingrs@gmail.com>
2022-11-20 18:54:30 -0600

    Menu Updated

--- Game/js/plugins/Naptora_TitleScreen.js
+++ Game/js/plugins/Naptora_TitleScreen.js
@@ -18,15 +18,55 @@
  * @desc Image to use as title
  * @default logo
  * @require 1
- * @dir img/
+ * @dir img/menu/
  * @type file
+ * 
+ * @param Title Image Factor
+ * @desc Image size factor (0-100)
+ * Default: 100
+ * @default 100
  * 
  */
 
 var params = PluginManager.parameters("Naptora_TitleScreen");
+var title_image_path = String(params["Title Image"]);
+var bg_image_path = String(params["Background"]);
+var title_factor = Number.parseInt(params["Title Image Factor"]);
 
 (function(){
 
+/* rewrite ImageManager for index "menu" folder */
+ImageManager.loadMenu = function(filename, hue) {
+    return this.loadBitmap('img/menu/', filename, hue, false);
+};
 
+/* Rewrite scene title of native rpgmaker*/
+
+// Set background
+Scene_Title.prototype.createBackground = function() {
+    this._backSprite1 = new Sprite(ImageManager.loadMenu(bg_image_path));
+    this._backSprite2 = new Sprite(ImageManager.loadTitle2($dataSystem.title2Name));
+    this.addChild(this._backSprite1);
+};
+
+// Set Title image
+Scene_Title.prototype.createForeground = function() {
+    this._gameTitleSprite = new Sprite(ImageManager.loadMenu(title_image_path));
+    // Resize title image
+    var factor = title_factor/100;
+    this._gameTitleSprite.scale.set(factor,factor);
+
+    this.addChild(this._gameTitleSprite);
     
+    // Center title
+    Scene_Title.prototype.centerSprite(this._gameTitleSprite);
+
+    // Climb title
+    this._gameTitleSprite.y = Graphics.height / 4;
+    this._gameTitleSprite.anchor.y = 0.5;
+};
+
+
+
+
 })();
