952e0c1b0ac75f66366caa1752ad3cfe24804e76
Hitorenti <lvicmdfs9@gmail.com>
2022-12-02 18:57:05 -0600

    gitignore fix

---
+++ application/Assets/Scripts/system/TS_Actions.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TS_Actions : MonoBehaviour
+{
+    public void New_Game()
+    {
+
+    }
+
+    public void Continue_Game()
+    {
+
+    }
+
+    public void Game_Settings()
+    {
+
+    }
+}
